3f5cefe65b2adabfaf9f578d4569d09602afec2e
alterecho <jelisavetadzi@gmail.com>
2023-01-06 02:52:09 +0200

    pre 2

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,5 +1,12 @@
 var filterContainer = document.getElementById("filterContainer");
 var isFilterContainerOpen = false;
+var artistName;
+var creationDate;
+var firstAlbum;
+var members;
+var concertDates;
+var concertLocations;
+var imageSrc;
 
 showFilters = function () {
     if(isFilterContainerOpen) {
@@ -43,3 +50,35 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+const modalCard = document.getElementById("modalCard")
+const button = document.querySelector("button")
+var span = document.getElementsByClassName("close-card")[0];
+
+span.onclick = function() {
+  modalCard.style.display = "none";
+}
+
+openModal = function(id) {
+  artistName = document.getElementById(`artistName_${id}`).innerHTML;
+  creationDate = document.getElementById(`creationDate_${id}`).innerHTML;
+  firstAlbum = document.getElementById(`firstAlbum_${id}`).innerHTML;
+  members = document.getElementById(`members_${id}`).innerHTML;
+  dates = document.getElementById(`dates_${id}`).innerHTML;
+  locations = document.getElementById(`locations_${id}`).innerHTML;
+  imageSrc = document.getElementById(`discographyImage_${id}`).src;
+
+  document.getElementById('artistNameModal').innerHTML = artistName;
+  document.getElementById('creationDateModal').innerHTML = creationDate;
+  document.getElementById('firstAlbumModal').innerHTML = firstAlbum;
+  document.getElementById('membersModal').innerHTML = members;
+  document.getElementById('datesModal').innerHTML = dates;
+  document.getElementById('locationsModal').innerHTML = locations;
+  document.getElementById('imageModal').src = imageSrc;
+
+  
+  modalCard.style.display = "block";
+}
+
+
+
