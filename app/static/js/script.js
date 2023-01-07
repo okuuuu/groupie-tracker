f0f0330c427bc8c2ad8a41f7fe1d4a8cc18ef7e9
alterecho <jelisavetadzi@gmail.com>
2023-01-07 03:14:59 +0200

    map

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -8,6 +8,8 @@ var concertDates;
 var concertLocations;
 var imageSrc;
 
+
+// Filters Drawer
 showFilters = function () {
     if(isFilterContainerOpen) {
         filterContainer.style.display = "none"
@@ -16,7 +18,6 @@ showFilters = function () {
     }
     isFilterContainerOpen = !isFilterContainerOpen;
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == filterContainer) {
@@ -24,42 +25,32 @@ window.onclick = function(event) {
   }
 }
 
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
+//Modal Credits
+var modal = document.getElementById("modalCredits");
+var button= document.getElementById("creditsButton");
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
+button.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// Modal Card
+var modalCard = document.getElementById("modalCard")
+var spanCard = document.getElementsByClassName("close-card")[0];
 
-const modalCard = document.getElementById("modalCard")
-const button = document.querySelector("button")
-var span = document.getElementsByClassName("close-card")[0];
-
-span.onclick = function() {
+spanCard.onclick = function() {
   modalCard.style.display = "none";
 }
 
 openModal = function(id) {
+  setTimeout(function () {
+    map.setZoom(1);
+    window.dispatchEvent(new Event('resize'));
+}, 1000);
   artistName = document.getElementById(`artistName_${id}`).innerHTML;
   creationDate = document.getElementById(`creationDate_${id}`).innerHTML;
   firstAlbum = document.getElementById(`firstAlbum_${id}`).innerHTML;
@@ -76,9 +67,42 @@ openModal = function(id) {
   document.getElementById('locationsModal').innerHTML = locations;
   document.getElementById('imageModal').src = imageSrc;
 
-  
   modalCard.style.display = "block";
 }
 
+// Map
+var locations = [
+  {
+    Name: 'Paris, France',
+    Latitude: 48.856614,
+    Longitude: 2.3522219,
+    Type: 'city',
+    Importance: 0.9
+  },
+  {
+    Name: 'New York, USA',
+    Latitude: 40.7127753,
+    Longitude: -74.0059728,
+    Type: 'city',
+    Importance: 0.9
+  },
+  {
+    Name: 'Tokyo, Japan',
+    Latitude: 35.6894875,
+    Longitude: 139.6917064,
+    Type: 'city',
+    Importance: 0.9
+  }
+];
 
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 6,
+    minZoom: 1,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
+locations.forEach(function(location) {
+  var marker = L.marker([location.Latitude, location.Longitude]).addTo(map);
+  marker.bindTooltip(location.Name, {permanent:false, direction:`right`});
+});
